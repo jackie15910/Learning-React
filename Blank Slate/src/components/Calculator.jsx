c450266baf8464b7f678a2bf31d7351dc6e2a1f8
Jackie Luc <jackie15910@gmail.com>
2025-03-12 15:15:38 -0400

    Blank slate template

---
+++ Blank Slate/src/components/Calculator.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react';
+import "../style.css"
+
+const Calculator = () => {
+    const [,] = useState([]);
+
+  return (
+  <div>
+
+  </div>
+  )
+};
+
+export default Calculator;

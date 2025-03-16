cba399414585e7740618f62836c6b97c695e1a1d
Jackie Luc <jackie15910@gmail.com>
2025-03-16 00:36:08 -0400

    Succesfull render

--- toggle background/src/components/Toggle.jsx
+++ toggle background/src/components/Toggle.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { useState } from 'react';
 import "../style.css"
 
+
+
 const Toggle = () => {
     const [backgroundColor,setBackgroundColor] = useState('white');
     const [textColor,setTextColor] = useState('#1b1b1b');
     const [buttonStyle,setButtonStyle] = useState('white');
+
+    const handleClick = () => {
+      setBackgroundColor(backgroundColor =='#1b1b1b' ? 'white' : '#1b1b1b');
+      setTextColor(textColor =='white' ? '#1b1b1b' : 'white');
+      setButtonStyle(buttonStyle == '#1b1b1b' ? 'white' : '#1b1b1b');
+  }
 
   return (
   <div style={{backgroundColor, color:textColor}}>
@@ -16,6 +24,12 @@ const Toggle = () => {
     >
     {backgroundColor === '#1b1b1b' ? 'Dark Mode' : 'Light Mode'}
     </button>
+
+    <section>
+      <h1>Welcome to the <br/>
+      Real World...</h1>
+      </section>
+      <p>Click the button to toggle the background color</p>
   </div>
   )
 };

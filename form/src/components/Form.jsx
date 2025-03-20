40733d44ef02731ea90c352c112730662828c498
Jackie Luc <jackie15910@gmail.com>
2025-03-20 14:28:29 -0400

    Added states

--- form/src/components/Form.jsx
+++ form/src/components/Form.jsx
@@ -3,7 +3,19 @@ import { useState } from 'react';
 import "../style.css"
 
 const Form = () => {
-    const [,] = useState([]);
+    const [username,setUsername] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [confirmPassword,setConfirmPassword] = useState('');
+    const [errorUsername,setErrorUsername] = useState('');
+    const [errorEmail,setErrorEmail] = useState('');
+    const [errorPassword,setErrorPassword] = useState('');
+    const [errorConfirmPassword,setErrorConfirmPassword] = useState('');
+
+    const [userColor, setUserColor] = useState('');
+    const [emailColor, setEmailColor] = useState('');
+    const [passwordColor, setPasswordColor] = useState('');
+    const [confirmPasswordColor, setConfirmPasswordColor] = useState('');
 
   return (
   <div>

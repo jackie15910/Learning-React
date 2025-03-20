bd6870439edab590a58be5d4c8cdfac6a08914ca
Jackie Luc <jackie15910@gmail.com>
2025-03-20 14:19:10 -0400

    Blank new form

---
+++ form/src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react';
+import "../style.css"
+
+const Form = () => {
+    const [,] = useState([]);
+
+  return (
+  <div>
+
+  </div>
+  )
+};
+
+export default Form;

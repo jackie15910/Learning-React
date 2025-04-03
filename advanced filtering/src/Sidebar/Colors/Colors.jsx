48ff07a635a027dd3d1f598f5e79d8a5141936c4
Jackie Luc <jackie15910@gmail.com>
2025-04-02 23:08:05 -0400

    Added colors

--- advanced filtering/src/Sidebar/Colors/Colors.jsx
+++ advanced filtering/src/Sidebar/Colors/Colors.jsx
@@ -3,6 +3,33 @@ import './Colors.css'
 
 export default function Colors() {
   return (
-    <div>Colors</div>
+    <div className="ml">
+    <h2 className="sidebar-title price-title">Price</h2>
+
+    <label className="sidebar-label-container">
+        <input type='radio' name="test2" />
+        <span className="checkmark"></span>All
+    </label>
+    <label className="sidebar-label-container">
+        <input type='radio' name="test2" />
+        <span className="checkmark"></span>Black
+    </label>
+    <label className="sidebar-label-container">
+        <input type='radio' name="test2" />
+        <span className="checkmark"></span>Blue
+    </label>
+    <label className="sidebar-label-container">
+        <input type='radio' name="test2" />
+        <span className="checkmark"></span>Red
+    </label>
+    <label className="sidebar-label-container">
+        <input type='radio' name="test2" />
+        <span className="checkmark"></span>Green
+    </label>
+    <label className="sidebar-label-container">
+        <input type='radio' name="test2" />
+        <span className="checkmark"></span>White
+    </label>
+  </div>
   )
 }

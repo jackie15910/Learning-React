a2d57fe56bb00f9cefe48538b71e3c2222f5a9d1
Jackie Luc <jackie15910@gmail.com>
2025-04-02 20:54:30 -0400

    Added categories

--- advanced filtering/src/Sidebar/Category/Category.jsx
+++ advanced filtering/src/Sidebar/Category/Category.jsx
@@ -3,6 +3,29 @@ import './Category.css'
 
 export default function Category() {
   return (
-    <div>Category</div>
+    <h2 className="sidebar-title">
+      <div>
+        <label className="sidebar-label-container">
+          <input type='radio' name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type='radio' name="test" />
+          <span className="checkmark"></span>Sneakers
+        </label>
+        <label className="sidebar-label-container">
+          <input type='radio' name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type='radio' name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+        <label className="sidebar-label-container">
+          <input type='radio' name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+      </div>
+    </h2>
   )
 }
--- advanced filtering/src/Sidebar/Sidebar.jsx
+++ advanced filtering/src/Sidebar/Sidebar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Sidebar.css'
+import Category from './Category/Category'
+import Price from './Price/Price'
+import Colors from './Colors/Colors'
 
 export default function Sidebar() {
   return (
@@ -8,6 +11,9 @@ export default function Sidebar() {
         <div className="logo-container">
           <h1>🛒</h1>
         </div>
+        <Category/>
+        <Price/>
+        <Colors/>
       </section>
     </>
   )
